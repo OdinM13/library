33d3e5f73a9621e4c6e9249e771a76bb6cab8140
OdinM13 <markodinius@gmail.com>
2026-01-26 21:27:38 +0100

    Build dialog/form, add some styles and function to it

--- main.js
+++ main.js
@@ -16,4 +16,26 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
+const btnBook = document.querySelector(".new-book");
+const dialog = document.querySelector("dialog");
+const closeButton = document.querySelector(".cancel");
+const submitButton = document.querySelector(".add");
 
+btnBook.addEventListener("click", () => {
+    dialog.showModal()
+    console.log("Click");
+})
+
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
+
+submitButton.addEventListener("click", () => {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+    addBookToLibrary(title, author, pages, read);
+    console.log(myLibrary);
+    //add deletion of form input, so when you click "add new book" the form is not prefilled with old input
+})
